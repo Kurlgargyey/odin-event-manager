66e4e829bac41cbb90215b78b2723949ca153a30
Kurlgargyey <nahsirkivar@gmail.com>
2022-05-02 15:30:18 +0200

    Add files via upload

---
+++ lib/event_manager.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'erb'
+require 'csv'
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def clean_phone_number(phone_number)
+  number = phone_number.to_s.delete '^0123456789'
+  if number.to_s.count('0123456789') < 10
+    '000-000-0000'
+  elsif number.to_s.count('0123456789') == 11 && number[0] == '1'
+    number[1..-1].insert(3, '-').insert(7, '-')
+  elsif number.to_s.count('0123456789') > 10
+    '000-000-0000'
+  else
+    number.insert(3, '-').insert(7, '-')
+  end
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+puts 'Event manager intialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name].capitalize
+
+  zipcode = clean_zipcode(row[:zipcode])
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+
+  phone_number = clean_phone_number(row[:homephone])
+  puts phone_number
+end
