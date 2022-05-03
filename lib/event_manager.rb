5df2dbf3a91120bd7108a2f95777ff1717ec1a35
Kurlgargyey <nahsirkivar@gmail.com>
2022-05-03 17:59:42 +0200

    all 3 assignments done

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+require 'time'
 require 'erb'
 require 'csv'
 require 'google/apis/civicinfo_v2'
+
+PHONE_NUMBER_PATH = 'output/phone_numbers.html'
+REGTIME_PATH = 'output/regtimes.html'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -19,6 +23,53 @@ def clean_phone_number(phone_number)
   else
     number.insert(3, '-').insert(7, '-')
   end
+end
+
+def save_phone_number(name, number)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  File.open(PHONE_NUMBER_PATH, 'a') do |file|
+    file.puts "#{name}: #{number}"
+  end
+end
+
+def save_reghours(hour, tally)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  File.open(REGTIME_PATH, 'a') do |file|
+    file.puts "Hour #{hour}: #{tally} Registrations"
+  end
+end
+
+def peak_reghours(times)
+  hours = times.map { |datetime| datetime.hour }
+  hours.tally.sort_by { |_hour, count| count }.reverse![0..1].each do |hour, tally|
+    save_reghours(hour, tally)
+  end
+end
+
+def save_regdays(day, tally)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  File.open(REGTIME_PATH, 'a') do |file|
+    file.puts "#{day}: #{tally} Registrations"
+  end
+end
+
+def peak_regdays(times)
+  wdays = times.map { |datetime| Date::DAYNAMES[datetime.wday] }
+  wdays.tally.sort_by { |_day, count| count }.reverse![0..1].each do |day, tally|
+    save_regdays(day, tally)
+  end
+end
+
+def regtimes(users)
+  arr = []
+  users.each do |row|
+    arr.push(DateTime.strptime(row[:regdate], '%y/%e/%m %k:%M'))
+  end
+  peak_reghours(arr)
+  peak_regdays(arr)
 end
 
 def legislators_by_zipcode(zipcode)
@@ -46,6 +97,28 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+def process_attendees(attendees)
+  template_letter = File.read('form_letter.erb')
+  erb_template = ERB.new template_letter
+
+  File.open(PHONE_NUMBER_PATH, 'w') {}
+
+  attendees.each do |row|
+    id = row[0]
+    name = row[:first_name].capitalize
+
+    zipcode = clean_zipcode(row[:zipcode])
+
+    legislators = legislators_by_zipcode(zipcode)
+
+    form_letter = erb_template.result(binding)
+
+    save_thank_you_letter(id, form_letter)
+
+    save_phone_number(name, clean_phone_number(row[:homephone]))
+  end
+end
+
 puts 'Event manager intialized!'
 
 contents = CSV.open(
@@ -54,21 +127,10 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
+process_attendees(contents)
 
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name].capitalize
+contents.rewind
 
-  zipcode = clean_zipcode(row[:zipcode])
+File.open(REGTIME_PATH, 'w') {}
 
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-
-  save_thank_you_letter(id, form_letter)
-
-  phone_number = clean_phone_number(row[:homephone])
-  puts row[:regdate]
-end
+regtimes(contents)
